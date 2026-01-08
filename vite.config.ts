877e926059fd1fb7d67029d07617ffda77069e83
Addonay <oaddonay@gmail.com>
2026-01-08 05:31:16 +0300

    allowed *.e2b.app hosts

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,9 @@ import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
-export default defineConfig({ plugins: [tailwindcss(), sveltekit(), devtoolsJson()] });
+export default defineConfig({
+  plugins: [tailwindcss(), sveltekit(), devtoolsJson()],
+  server: {
+    allowedHosts: ['.e2b.app', '.e2b.dev']
+  }
+});
